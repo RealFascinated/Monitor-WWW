072e56890ee584346f83e7a41bb570bd4b5f7ce4
fascinated <liam@fascinated.cc>
2026-06-14 07:20:51 +0100

    Fix missing cn import on servers table columns

--- src/components/user/server-table-columns.tsx
+++ src/components/user/server-table-columns.tsx
@@ -24,6 +24,7 @@ import {
   SERVER_TABLE_COLUMN_TOOLTIPS,
 } from "@/lib/tooltips/copy"
 import { defaultMetricRangeSearch } from "@/lib/metrics/default-range"
+import { cn } from "@/lib/utils"
 
 export type ServerTableRow = {
   serverId: number
